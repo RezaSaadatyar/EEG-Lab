f0217f7e015cc455ff36947dc359a7df77eb5927
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2023-08-16 09:48:26 +0330

    Update Channel_Locations.py

--- Code/Channel_Locations.py
+++ Code/Channel_Locations.py
@@ -1,5 +1,4 @@
 # ================================== Importing the required Libraries ===========================
-import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
